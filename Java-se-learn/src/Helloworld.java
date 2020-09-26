702e2c25095b930fd5657e167db2d2f341797304
Tarun <Tarun@Unknown>
2020-09-26 11:15:16 +0530

    print messages * multi line * multi line in single statement * formatted print

--- Java-se-learn/src/Helloworld.java
+++ Java-se-learn/src/Helloworld.java
@@ -13,5 +13,18 @@ public class Helloworld
     public static void main(String[] args)
     {
         System.out.println("Welcome to JAVA! Let's do the coding.");
+        
+        // multiline print
+        System.out.print("Welcome to JAVA! ");
+        System.out.println("Let's do the coding.");
+        System.out.print("Welcome to JAVA! ");
+        System.out.println("Let's do the coding.");
+        
+        // multiline print in one line
+        System.out.println("Welcome to JAVA!\nLet's do the coding.");
+        
+        // formatted print
+        System.out.printf("%s%n%s%n", "Welcome to JAVA", "Let's do the coding.");
+        
     }
 }
